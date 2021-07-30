fe6f26bc836cc6f023bf428346d21f6b0f1c2aaa
Divakarjha-bit <jhadivakar55@gmail.com>
2021-07-30 11:52:22 +0530

    first commit

---
+++ javascriptfile/index.js
@@ -0,0 +1,13 @@
+function hire(){
+    alert("contact on this number : 6289128773"+"\n"+"mail on : jhadivakarkumar77@gmail.com");
+}
+function about()
+{
+    alert("whatshapp on 6290688546"+"\n"+"mail on jhadivakarkumar77@gmail.com");
+}
+
+function cont()
+{
+    var str=prompt("Hii what's your good name");
+    alert("Hii"+" "+str+"."+"\n"+"please contact on this number: 6289128773"+"\n"+"mail us: jhadivakarkumar77@gmail.com");
+}
